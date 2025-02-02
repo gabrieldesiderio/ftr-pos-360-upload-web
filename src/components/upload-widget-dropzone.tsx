07fa3ad663168da1c7172d99e56c2fbf909eea1b
Gabriel Desidério <gabrieldesd7@gmail.com>
2025-02-02 18:21:52 -0300

    visual states

--- src/components/upload-widget-dropzone.tsx
+++ src/components/upload-widget-dropzone.tsx
@@ -1,13 +1,17 @@
 import { useDropzone } from 'react-dropzone'
+import { CircularProgressBar } from './ui/circular-progress-bar'
 
 export function UploadWidgetDropdzone() {
+  const isThereAnyPendingUpload = true
+  const uploadGlobalPercentage = 66
+
   const { getRootProps, getInputProps, isDragActive } = useDropzone({
     multiple: true,
     accept: {
       'image/jpeg': [],
       'image/png': [],
     },
-    onDrop(acceptedFiles, filesRejection, event) {
+    onDrop(acceptedFiles) {
       console.log(acceptedFiles)
     }
   })
@@ -19,10 +23,20 @@ export function UploadWidgetDropdzone() {
       {...getRootProps()}
     >
       <input type="file" {...getInputProps()} />
-      <span className='text-xs'>Drop your files here or</span>
-      <span className='text-xs underline'>click to open picker</span>
+      
+      {isThereAnyPendingUpload ? (
+        <div className='flex flex-col gap-2.5 items-center'>
+          <CircularProgressBar progress={uploadGlobalPercentage} size={56} strokeWidth={4} />
+          <span className='text-xs'>Uploading 2 files...</span>
+        </div>
+      ) : (
+        <>
+          <span className='text-xs'>Drop your files here or</span>
+          <span className='text-xs underline'>click to open picker</span>
+        </>
+      )}
     </div>
 
-    <span className='text-xs text-zinc-400'>Only PNG and JPG are supported.</span>
+    <span className='text-xxs text-zinc-400'>Only PNG and JPG are supported.</span>
   </div>
 }
